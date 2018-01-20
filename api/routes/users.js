3372aa1e6dd54fa2043c2f12396cf2dc7b7d91c6
beltranbot <canti89@gmail.com>
2018-01-20 10:28:43 -0500

    added login functionality

---
+++ api/routes/users.js
@@ -0,0 +1,135 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+router.post('/signup', (req, res, next) => {
+  User.find({email: req.body.email})
+    .exec()
+    .then(users => {
+      if (users.length >= 1) {
+        return res.status(409).json({
+          message: 'Email already registered'
+        })
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) return res.status(500).json({ error: err })
+          const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: req.body.email,
+            password: hash
+          })
+          User.save()
+            .then(result => {
+              console.log(result)
+              res.status(201).json({
+                message: 'User Created'
+              })
+            })
+            .catch(err => {
+              console.log(err)
+              res.status(500).json({ error: err })
+            })
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    })
+})
+
+router.get('/', (req, res, next) => {
+  User.find()
+    .exec()
+    .then(result => {
+      console.log(result)
+      res.status(200).json({
+        count: result.length,
+        users: result
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    })
+})
+
+router.post('/login', (req, res, next) => {
+  User.find({email: req.body.email})
+    .exec()
+    .then(users => {
+      if (users.length < 1) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        })
+      }
+      bcrypt.compare(req.body.password, users[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: 'Auth failed'
+          })
+        }
+        if (result) {
+          const token = jwt.sign({
+            email: users[0].email,
+            userId: users[0]._id,
+          },
+          process.env.JWT_KEY,
+          {
+            expiresIn: '1h'
+          }
+        )
+          return res.status(200).json({
+            message: 'Auth successful',
+            token
+          })
+        }
+        return res.status(200).json({
+          message: 'Auth failed'
+        })
+      }) 
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        errors: err
+      })
+    })
+})
+
+router.get('/:userId', (req, res, next) => {
+  const id = req.params.userId 
+  User.findById(id)
+    .exec()
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    })
+})
+
+router.delete('/:userId', (req, res, next) => {
+  const id = req.params.userId
+  User.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'User deleted',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/users',
+          body: { email: 'String', password: 'String' }
+        }
+      })
+    }).catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    })
+})
+
+module.exports = router
--- app.js
+++ app.js
@@ -30,9 +30,11 @@ app.use((req, res, next) => {
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/users')
 
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
+app.use('/users', userRoutes)
 
 app.use((req, res, next) => {
   const error = new Error('Not Found')
